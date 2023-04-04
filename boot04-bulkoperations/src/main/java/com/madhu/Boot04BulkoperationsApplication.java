48aa4426807ff3926b516b5e157d2d992e1e19ae
madhu <madhu.java.git@gmail.com>
2023-04-04 17:09:28 +0530

    commit

--- boot04-bulkoperations/src/main/java/com/madhu/Boot04BulkoperationsApplication.java
+++ boot04-bulkoperations/src/main/java/com/madhu/Boot04BulkoperationsApplication.java
@@ -6,6 +6,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 
 import com.madhu.comp.Company;
+import com.madhu.comp.Company2;
 
 @SpringBootApplication
 public class Boot04BulkoperationsApplication {
@@ -14,6 +15,9 @@ public class Boot04BulkoperationsApplication {
 		ApplicationContext context = SpringApplication.run(Boot04BulkoperationsApplication.class, args);
 	Company company = context.getBean(Company.class);
 	System.out.println(company);
+	
+	Company2 company2 = context.getBean(Company2.class);
+	System.out.println(company2);
 		
 		((ConfigurableApplicationContext) context).close();
 	
--- boot04-bulkoperations/src/main/java/com/madhu/comp/Company.java
+++ boot04-bulkoperations/src/main/java/com/madhu/comp/Company.java
@@ -1,10 +1,12 @@
 package com.madhu.comp;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationProperties(prefix="org.info")
+//@PropertySource(value="in/madhu/commons/input.properties")
 
 public class Company {
 private String name;
